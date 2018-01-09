c388e7322b46f7a175242ed5ebee7ab513964d4f
Maurits Lam <mhjlam@users.noreply.github.com>
2018-01-09 14:55:37 +0100

    Update string.h

--- string.h
+++ string.h
@@ -14,12 +14,12 @@ extern void 	string_init_clear(string *str);
 extern void 	string_free(string*);
 
 extern char 	string_get(string*, size_t);
-extern void		string_append(string*, const string*);
-extern string*	string_concat(const string*, const string*);
+extern void 	string_append(string*, const string*);
+extern string* 	string_concat(const string*, const string*);
 extern bool 	string_compare(const string*, const string*);
-extern void		string_copy(string*, const string*);
+extern void 	string_copy(string*, const string*);
 extern size_t 	string_length(const string*);
 
-extern string*	string_substring(const string*, size_t, size_t);
-extern size_t	string_find(const string*, const string*);
-extern void		string_print(const string*);
+extern string* 	string_substring(const string*, size_t, size_t);
+extern size_t 	string_find(const string*, const string*);
+extern void 	string_print(const string*);
