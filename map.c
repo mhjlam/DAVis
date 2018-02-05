a16332f9c127edd468f8897a2fea00edd41abdfa
Maurits Lam <mhjlam@gmail.com>
2018-02-05 23:18:46 +0100

    Map struct skelly

---
+++ map.c
@@ -0,0 +1,51 @@
+#include "map.h"
+
+map* map_new()
+{
+    return NULL;
+}
+
+void map_free(map *m)
+{
+
+}
+
+void* map_get(map *m, const char *key)
+{
+    return NULL;
+}
+
+pair* map_find(map *m, const char *key)
+{
+    return NULL;
+}
+
+void map_insert(map *m, const char *key, void *value)
+{
+
+}
+
+void map_remove(map *m, const char *key)
+{
+
+}
+
+void map_print_s(map *m)
+{
+
+}
+
+void map_print_i(map *m)
+{
+
+}
+
+void map_format_i(map *m, char **fmt)
+{
+
+}
+
+void map_format_s(map *m, char **fmt)
+{
+
+}
---
+++ map.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct pair
+{
+    char        *key;
+    void        *value;
+	struct pair *next;
+} pair;
+
+typedef struct bucket
+{
+    size_t  size;
+    pair  *pairs;
+} bucket;
+
+typedef struct map
+{
+	size_t  size;
+	bucket *buckets;
+} map;
+
+extern map*     map_new();
+extern void     map_free(map*);
+
+extern void*    map_get(map*, const char*);
+extern pair*    map_find(map*, const char*);
+extern void     map_insert(map*, const char*, void*);
+extern void     map_remove(map*, const char*);
+
+extern void     map_print_s(map*);
+extern void     map_print_i(map*);
+extern void     map_format_i(map*, char**);
+extern void     map_format_s(map*, char**);
